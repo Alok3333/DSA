465f6a2e11d2564d9dda892e50e954ab89c8f880
Alok3333 <113538469+Alok3333@users.noreply.github.com>
2023-02-16 18:52:47 +0530

    Update selectionSort.js

--- sorting_parts/selectionSort.js
+++ sorting_parts/selectionSort.js
@@ -1,6 +1,6 @@
 // selection sort
 // In this algorithm we are selecting one element and searching an array list. which is smallest element
-// in an array if we have finded then swap the element.
+// in an array if we have founded then swap the element.
 
 let arr = [16, 2, 10, 7, 3, 1, 5];
 
