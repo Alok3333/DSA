dee45d9c6cc1a852c96cfbb0d8f1d4b946c9fded
Alok3333 <alokprajapati697@gmail.com>
2023-06-01 11:51:08 +0530

    created a switch statement to check fruits

---
+++ loop/switch.js
@@ -0,0 +1,17 @@
+let fruit = 'Mango';
+
+switch (fruit) {
+  case 'Apple':
+    console.log("It's a red sweet.");
+    break;
+  case 'Mango':
+    console.log('Fruits of king.');
+    break;
+  case 'Orange':
+    console.log("It's Orange.");
+  case 'Guava':
+    console.log("It's a Amrud.");
+    break;
+  default:
+    console.log('This fruit is not avilable.');
+}
